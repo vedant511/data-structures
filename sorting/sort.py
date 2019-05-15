288ebdb148e3c43ccd694901d4f8be52871189ad
Vedant Sharma <vedantsharma511@gmail.com>
2019-05-15 14:44:28 -0700

    Completed sorting algorithms

--- sorting/sort.py
+++ sorting/sort.py
@@ -3,6 +3,8 @@ class Sort(object):
         pass
 
     def bubble_sort(self, lst):
+        if len(lst) <= 1:
+            return lst
         for i in range(len(lst)-1):
             j, k = 0, 1
             swapped = False
@@ -20,6 +22,8 @@ class Sort(object):
         return lst
 
     def selection_sort(self, lst):
+        if len(lst) <= 1:
+            return lst
         for i in range(len(lst)-1):
             max_idx = 0
             j = 0
@@ -30,17 +34,68 @@ class Sort(object):
                 j += 1
         return lst
 
-    def insertion_sort(self):
-        pass
+    def insertion_sort(self, lst):
+        if len(lst) <= 1:
+            return lst
+        for i in range(1, len(lst)):
+            current = i
+            value = lst[i]
+            while current > 0 and lst[current-1] > value:
+                lst[current] = lst[current-1]
+                current -= 1
+            lst[current] = value
+        return lst
 
-    def shell_sort(self):
-        pass
+    def merge_sort(self, lst):
+        if len(lst) <= 1:
+            return lst
+        mid = len(lst) // 2
+        left_lst = self.merge_sort(lst[:mid])
+        right_lst = self.merge_sort(lst[mid:])
+        return self.__merge(left_lst, right_lst)
 
-    def quick_sort(self):
-        pass
+    def __merge(self, lst1, lst2):
+        i = 0
+        j = 0
+        new_lst = []
+        while i < len(lst1) and j < len(lst2):
+            if lst1[i] < lst2[j]:
+                new_lst.append(lst1[i])
+                i += 1
+            else:
+                new_lst.append(lst2[j])
+                j += 1
+        while i < len(lst1):
+            new_lst.append(lst1[i])
+            i += 1
+        while j < len(lst2):
+            new_lst.append(lst2[j])
+            j += 1
+        return new_lst
 
-    def merge_sort(self):
-        pass
+    def quick_sort(self, lst):
+        return self.__quick_sort_helper(lst, 0, len(lst)-1)
 
-    def merge(self):
-        pass
+    def __quick_sort_helper(self, lst, left, right):
+        if left < right:
+            split = self.__partition(lst, left, right)
+            self.__quick_sort_helper(lst, left, split-1)
+            self.__quick_sort_helper(lst, split+1, right)
+        return lst
+
+    def __partition(self, lst, start, end):
+        pivot = lst[start]
+        left_ptr = start + 1
+        right_ptr = end
+        done = False
+        while not done:
+            while left_ptr <= right_ptr and lst[left_ptr] <= pivot:
+                left_ptr += 1
+            while right_ptr >= left_ptr and lst[right_ptr] >= pivot:
+                right_ptr -= 1
+            if right_ptr < left_ptr:
+                done = True
+            else:
+                lst[left_ptr], lst[right_ptr] = lst[right_ptr], lst[left_ptr]
+        lst[start], lst[right_ptr] = lst[right_ptr], lst[start]
+        return right_ptr
